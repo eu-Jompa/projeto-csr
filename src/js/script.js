d719e133b0c41c9ef2c633c7aca04ccd572e67a7
JotaMacLane <jpnascimentoa@gmail.com>
2024-08-22 22:46:06 -0300

    Atualizando Projeto

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector("fa-solid fa-bars");
+const closeMenu = document.querySelector("fa-solid fa-x");
+
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click,");
+
+function onMenu(){
+    nav.style.left("100vw");
+    
+
+}
